2fae51c22f83fc8fbc222a7f676969ca1fddbce0
ayush-1560 <ayush.15609514@gmail.com>
2025-07-21 00:27:21 +0530

    removed the previous changes

--- server/server.js
+++ server/server.js
@@ -5,12 +5,10 @@ import { clerkMiddleware,requireAuth } from '@clerk/express'
 import aiRouter from './routes/aiRoutes.js';
 import connectCloudinary from './configs/cloudinary.js';
 import userRouter from './routes/userRoutes.js';
-import clerkWebhook from './routes/webhook/clerk.js';
 const app = express();
 
 await connectCloudinary();
 app.use(cors());
-app.use('/api/webhook', clerkWebhook);
 app.use(express.json());
 app.use(clerkMiddleware());
 app.use('/api/ai',aiRouter);
